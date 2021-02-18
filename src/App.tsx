d87640d6a899e1d6e6b7079b816e3d366bd47c8b
zpr1g <yuns.xie@qq.com>
2021-02-18 21:33:54 +0800

    feat: code lint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,18 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <p>Hello Vite + React!</p>
         <p>
-          <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
+          <button onClick={() => setCount((prevCount) => prevCount + 1)}>count is: {count}</button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             Learn React
           </a>
           {' | '}

c7caa2eac799c9b1566de5d1075a0ad2bdde11f5
theprimone <yuns.xie@qq.com>
2021-03-01 22:57:05 +0800

    fix: project root

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src',
   base: '/vite-react/',
   plugins: [reactRefresh()],
   server: {
     open: true,
+    port: 8000,
   },
 });

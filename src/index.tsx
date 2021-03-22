cd620f0a45dfb854f2805a875bd8dd2556c8f411
theprimone <yuns.xie@qq.com>
2021-03-22 22:57:23 +0800

    fix: runContent

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import './runConcent';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import renderRoutes from '@/core/renderRoutes';
 import routes from '@/routes';
 import config from '../config/config';
-import './runConcent';
 import './index.less';
 
 ReactDOM.render(

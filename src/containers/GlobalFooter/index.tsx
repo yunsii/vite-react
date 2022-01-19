c782fb14930e0f4e2a9c0aa1156a7313c1154654
theprimone <yuns.xie@qq.com>
2022-01-19 22:45:31 +0800

    chore: rename author

--- src/containers/GlobalFooter/index.tsx
+++ src/containers/GlobalFooter/index.tsx
@@ -1,15 +1,17 @@
 import { DefaultFooter } from '@ant-design/pro-layout';
 import { GithubOutlined } from '@ant-design/icons';
 
+const authorId = 'yunsii';
+
 export default function GlobalFooter() {
   return (
     <DefaultFooter
-      copyright={`${new Date().getFullYear()} theprimone 出品`}
+      copyright={`${new Date().getFullYear()} ${authorId} 出品`}
       links={[
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/theprimone/vite-react',
+          href: `https://github.com/${authorId}/vite-react`,
           blankTarget: true,
         },
       ]}

198a4aa875665129af7eaf40160f1b375b3dadee
yunsii <yunsii@users.noreply.github.com>
2022-03-18 14:18:36 +0000

    deploy: 57ef79c59f2839b205526e310d07ded8885baf29

---
+++ assets/404.e1fb7f4e.js
@@ -0,0 +1,1 @@
+import"./index.753b4101.js";import{B as o}from"./button.f48cfa66.js";import{R as s}from"./index.ad087f0f.js";import{l as t,n as e}from"./react-venders.71809d19.js";const p=()=>t(s,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t(o,{type:"primary",onClick:()=>e.push("/"),children:"Back Home"})});export{p as default};
---
+++ assets/RootLayout.0169370c.js
@@ -0,0 +1,1 @@
+import{l as a,F as r}from"./react-venders.71809d19.js";const s=({children:t})=>a(r,{children:t});export{s as default};

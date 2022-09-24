3c4858c2e7a90b614d27691e17739d3cca9e12fc
yuns <yuns.xie@qq.com>
2022-09-24 12:46:45 +0800

    chore: use pnpm and update vitjs

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
         'react',
         {
           react: [
-            'useImperativeHandle',
             'createElement',
             'cloneElement',
             'createContext',
